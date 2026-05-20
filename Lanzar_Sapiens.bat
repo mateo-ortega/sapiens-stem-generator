0ae544cd013907bbd67690471785514d108875d3
Mateo Ortega <teo.ritmos@gmail.com>
2026-05-19 20:51:09 -0500

    Delete Lanzar_Sapiens.bat

--- Lanzar_Sapiens.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:: Este archivo funciona como un boton directo. Ejecuta la aplicacion sin mostrar una ventana de consola molesta.
-cd /d "%~dp0"
-start "" "venv\Scripts\pythonw.exe" "main.py"
